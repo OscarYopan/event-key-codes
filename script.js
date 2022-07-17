9f299533a804340c5e0784de8cf134482edc8cd6
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-07-17 12:00:25 -0300

    -

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const containerActive = document.querySelector('.container')
 const btnActive = document.querySelector('.btn')
+let key = document.querySelector('#key')
+let keyCode = document.querySelector('#key-code')
+let eventCode = document.querySelector('#event-code')
+let Keyboard = '--'
 
 btnActive.addEventListener('click', () =>{
   containerActive.classList.remove('active')
   btnActive.classList.add('active')
+  key.innerHTML = `${Keyboard}`
+  keyCode.innerHTML = `${Keyboard}`
+  eventCode.innerHTML = `${Keyboard}`
+})
+
+key.addEventListener('KeyboardEvent()', () => {
+  key.innerHTML = `${Keyboard}`
+  keyCode.innerHTML = `${Keyboard}`
+  eventCode.innerHTML = `${Keyboard}`
 })
