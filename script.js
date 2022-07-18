db0a9f872713a699603c68864af88fb4deaa6c1c
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-07-18 14:40:36 -0300

    -

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-const containerActive = document.querySelector('.container')
+const dataBoxActive = document.querySelectorAll('.data-box')
 const btnActive = document.querySelector('.btn')
 let key = document.querySelector('#key')
 let keyCode = document.querySelector('#key-code')
 let eventCode = document.querySelector('#event-code')
 let Keyboard = '--'
 
-btnActive.addEventListener('click', () =>{
-  containerActive.classList.remove('active')
+window.addEventListener('keydown', (event) => {
+  dataBoxActive.classList.remove('active')
   btnActive.classList.add('active')
-  key.innerHTML = `${Keyboard}`
-  keyCode.innerHTML = `${Keyboard}`
-  eventCode.innerHTML = `${Keyboard}`
+  key.innerHTML = `${event.key === ' ' ? 'Space' : event.key}`
+  keyCode.innerHTML = `${event.keyCode}`
+  eventCode.innerHTML = `${event.eventCode}`
 })
+
+// btnActive.addEventListener('click', (event) =>{
+//   dataBoxActive.classList.remove('active')
+//   btnActive.classList.add('active')
+//   key.innerHTML = `${event.key === ' ' ? 'Space' : event.key}`
+//   keyCode.innerHTML = `${event.keyCode}`
+//   eventCode.innerHTML = `${event.eventCode}`
+// })
 
 // key.addEventListener('KeyboardEvent()', () => {
 //   key.innerHTML = `${Keyboard}`
